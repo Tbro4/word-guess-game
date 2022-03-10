2aaf5f1785e49947e18a8f343e435c166336101f
Tyler Brooks <thetylerbrooks@gmail.com>
2022-03-10 16:13:16 -0700

    Clicking start displays random array word

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,15 @@
 //Variables that point to references in html (usind ID or Class) using document.querySelector
 
 //this will need an eventListener
-var startButton = document.querySelector("start-button");
-var wordDisplay = document.querySelector("word-blanks");
+var startButton = document.querySelector(".start-button");
+var wordDisplay = document.querySelector(".word-blanks");
 //refers to span with total wins/losses
-var win = document.querySelector("win");
-var lose = document.querySelector("lose");
+var win = document.querySelector(".win");
+var lose = document.querySelector(".lose");
 //this will need an eventListener
-var resetButton = document.querySelector("reset-button");
+var resetButton = document.querySelector(".reset-button");
 //displays numeric value of seconds left
-var timerCount = document.querySelector("timer-count");
+var timerCount = document.querySelector(".timer-count");
 
 var wins = 0;
 var losses = 0;
@@ -27,3 +27,12 @@ var wordList = [
   "flute",
 ];
 var chosenWord;
+
+//program chooses a word from the array and returns it with .split()
+function wordChooser() {
+  chosenWord = wordList[Math.floor(Math.random() * wordList.length)];
+  console.log(chosenWord);
+  wordDisplay.textContent = chosenWord;
+}
+
+startButton.addEventListener("click", wordChooser);
