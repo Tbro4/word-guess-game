8986979406ce6b73680a45edff6e2e5371b98561
Tyler Brooks <thetylerbrooks@gmail.com>
2022-03-10 23:31:24 -0700

    diplaying underscores, collecting key presses after start and changing underscores back to letter when pressed

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,13 +26,48 @@ var wordList = [
   "trumpet",
   "flute",
 ];
-var chosenWord;
 
-//program chooses a word from the array and returns it with .split()
+//program chooses a word from the array wordList
 function wordChooser() {
-  chosenWord = wordList[Math.floor(Math.random() * wordList.length)];
-  console.log(chosenWord);
-  wordDisplay.textContent = chosenWord;
+  var randomWord = wordList[Math.floor(Math.random() * wordList.length)];
+  //   console.log(randomWord);
+  return randomWord;
 }
 
-startButton.addEventListener("click", wordChooser);
+function init() {
+  //brings in the word chosen from wordChooser
+  var chosenWord = wordChooser();
+  console.log(chosenWord);
+  //splits the word into its letters as an array (this will be used to compare against key presses)
+  var subStringArray = chosenWord.split("");
+  console.log(subStringArray);
+  //identical substring that we can change
+  var subStringMutated = chosenWord.split("");
+  console.log(subStringMutated);
+
+  //loops through substring and changes each character to underscore (HOWEVER, THIS IS MUTATING THE ARRAY, so how can we compare keystrokes?)
+  for (var i = 0; i < subStringMutated.length; i++) {
+    subStringMutated[i] = "_";
+  }
+
+  //displayd the rejoined word as all underscores
+  wordDisplay.textContent = subStringMutated.join(" ");
+
+  //starts to listen for key presses. IF the key matches a letter in the word, change the underscore back to that letter
+  document.addEventListener("keydown", function (event) {
+    var key = event.key.toLowerCase();
+    console.log(key);
+    for (var i = 0; i < subStringArray.length; i++) {
+      if (key === subStringArray[i]) {
+        subStringMutated[i] = key;
+        wordDisplay.textContent = subStringMutated.join(" ");
+      }
+    }
+  });
+}
+
+startButton.addEventListener("click", init);
+
+//look at #15 Keyboard events for keydown info
+
+//refer to #19 for the attributes for the displayed word will switch between the letter or a underline symbol
